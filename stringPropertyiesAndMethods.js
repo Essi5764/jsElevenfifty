d239ae7522aa4b4d5b01068fefa72476ee88554f
Essi Monneus <essimonneus@gmail.com>
2021-04-27 15:04:05 -0400

    update

---
+++ stringPropertyiesAndMethods.js
@@ -0,0 +1,30 @@
+//comparison
+
+//Equal to
+'3' == 3; 
+console.log('3' == 3);
+
+//Strict Equal to
+console.log('2' ==3);
+
+r += 1;//r = r + 1;
+
+r += 1;//r = r + 1;
+
+//Multiplication Operator
+r *= 1; //r = r *
+
+//Division operator
+r /= 1; //r = r /= 1
+
+//Remender (Modulus)
+r %= 1; //r = r %= 1
+
+// Exponential Operator
+r ** 1; //r = r ** 1
+
+
+
+
+
+
